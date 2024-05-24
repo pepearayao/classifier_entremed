eee57540604b863a03e87b04576b3beef322e235
Pepe Araya <pepearayao@gmail.com>
2024-05-24 14:11:07 +0200

    Improved tests cases

--- tests/specialties/test_specialties.py
+++ tests/specialties/test_specialties.py
@@ -1,9 +1,1 @@
-from normalizer.match.global_matchers import simple_regex_search, multiple_patterns_match, find_all_matches
-
-def test_simple_regex_search(jobpost_simple, specialties):
-    assert simple_regex_search(specialties['Enfermería'][0],jobpost_simple['title'])
-    assert not simple_regex_search(specialties['Kinesiología'][0],jobpost_simple['title'])
-
-def test_multiple_patterns_match(jobpost_simple, specialties):
-    assert multiple_patterns_match(specialties['Enfermería'],jobpost_simple['title'])
-    assert not multiple_patterns_match(specialties['Kinesiología'],jobpost_simple['title'])
+from normalizer.match.global_matchers import simple_regex_search, multiple_patterns_match
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,22 @@
-import json
-import os
+from normalizer.match.global_matchers import simple_regex_search, multiple_patterns_match, find_all_matches
+import pandas as pd
 
-def test_large_data(jobpost_simple):
-    print(jobpost_simple.keys())
-    assert len(jobpost_simple) == 8
+def test_large_data(raw_data):
+    assert len(raw_data['title']) == 20
+
+def test_simple_regex_search(raw_data, specialties):
+    pattern = specialties[0]['regex'][0]
+    sample = raw_data['title'][0]
+
+    assert simple_regex_search(pattern,sample).group() == 'ENFERMERA'
+
+def test_multiple_patterns_match(raw_data):
+    sample_description = raw_data['description'][0]
+    assert sorted(multiple_patterns_match(
+        ["enfermer.?","kinesi.log.?","kinesiolog.?","fisioterapia","mesoterapia","erg.nomo","ergono[ií]a"],
+        sample_description
+        )) == sorted(['KINESIOLOGO','ENFERMERA', 'Enfermerí'])
+
+    sample_description = raw_data['description'][1]
+    assert sorted(multiple_patterns_match(
+        ["iquique"],sample_description)) == sorted(['Iquique'])
