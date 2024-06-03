369f102480103031eefb79d2e11e21ea491c42a7
Pepe Araya <pepearayao@gmail.com>
2024-06-03 23:14:52 +0200

    Done test cases and Makefile comands

--- Makefile
+++ Makefile
@@ -4,8 +4,36 @@ reinstall_package:
 	@pip install -e .
 
 
+#################### TESTS ###################
 all_test:
 	@pytest -vv
 
 data_type_test:
-	@pytest -v tests/jobs_basics -k 'data_type'
+	@pytest -v -k 'data_type'
+
+specialty_test:
+	@pytest -v -k 'specialty'
+
+location_test:
+	@pytest -v -k 'location'
+
+job_schedule_test:
+	@pytest -v -k 'job_schedule'
+
+job_shift_test:
+	@pytest -v -k 'job_shift'
+
+job_engagement_test:
+	@pytest -v -k 'job_engagement'
+
+job_company_test:
+	@pytest -v -k 'company'
+
+job_accesibility_test:
+	@pytest -v -k 'inclusive'
+
+job_certificates_test:
+	@pytest -v -k 'certificates'
+
+job_emails_test:
+	@pytest -v -k 'emails'
---
+++ tests/jobs_details/test_job_accesibility.py
@@ -0,0 +1,33 @@
+from normalizer.match.job_details import Matcher
+from tests.fixtures.fixtures import data_for_param_tests
+import pytest
+
+def test_job_inclusive_offer_data_type(raw_data):
+    title = raw_data[0][0]
+    work_schedule = raw_data[0][7]
+    description = raw_data[0][14]
+    requisites = raw_data[0][15]
+    pills = raw_data[0][16]
+
+    result = Matcher().get_job_inclusive_offer([
+        title,
+        work_schedule,
+        description,
+        requisites,
+        pills
+    ])
+
+    assert type(result) == bool
+
+@pytest.mark.parametrize('data', data_for_param_tests(), ids=lambda x: x[0][18])
+def test_job_inclusive_offer_result(data):
+    raw_data = data[0]
+    labeled_data = data[1]
+    result = Matcher().get_job_inclusive_offer([
+        raw_data[0],
+        raw_data[7],
+        raw_data[14],
+        raw_data[15],
+        raw_data[16]
+    ])
+    assert result == labeled_data[8]
